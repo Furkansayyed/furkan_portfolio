bb4a609f5ac1c8de9d4cc12642ac3ae5cf541672
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-12-05 00:30:11 +0530

    minor changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
 
             {/* container */}
 
-            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <div className='max-w-[1000px] mx-auto px-8 md:px-28 lg:px-8 flex flex-col justify-center h-full'>
                 <p className='text-pink-600'>Hi, my name is </p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Furkan Sayyed</h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
