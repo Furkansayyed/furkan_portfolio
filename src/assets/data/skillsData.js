c3d9f08bbb7d2f770a8df2fff431b670dc733110
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-12-05 00:33:59 +0530

    created a data file for skill data and added tech icon files

---
+++ src/assets/data/skillsData.js
@@ -0,0 +1,23 @@
+import rlogo from '../icons/react-original-wordmark.svg'
+import blogo from '../icons/bootstrap-plain-wordmark.svg'
+import jlogo from '../icons/javascript-original.svg'
+
+const techStack = [
+    {
+        id:"01",
+        techIcon:rlogo,
+        name:'React'
+    },
+    {
+        id:"02",
+        techIcon:blogo,
+        name:'BootStrap'
+    },
+    {
+        id:"03",
+        techIcon:jlogo,
+        name:'Javascript'
+    }
+]
+
+export default techStack;
