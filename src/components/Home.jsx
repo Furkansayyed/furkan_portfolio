3a22cfcb956dc53aaac712fef383bc5228fc8443
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-12-05 23:43:37 +0530

    added link for scroll in home section.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Typewriter } from 'react-simple-typewriter'
+import { Link } from 'react-scroll';
 
 export default function Home() {
     return (
@@ -27,8 +28,9 @@ export default function Home() {
                 <div>
                     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
                         <span className='group-hover:rotate-90 duration-300'>
-                            <HiArrowNarrowRight className='ml-3' />
+                            <Link to="work" smooth={true} duration={500}> <HiArrowNarrowRight className='ml-3' /> </Link>
                         </span>
+
                     </button>
                 </div>
             </div>
