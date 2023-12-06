3182d8169c08b3f336a9409c6c74c71b9096457c
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-12-06 13:15:08 +0530

    Done with Projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contact from './components/Contact';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
+import Projects from './components/projects/Projects';
 import Work from './components/projects/Work';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <Home />
       <About />
       <Skills/>
-      <Work/>
+      <Projects/>
       <Contact/>
     </>
   );
